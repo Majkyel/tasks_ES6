72cad902dc482375e11403bd4789a87c3ef6fd97
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-27 13:57:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-const [...rest] = grades;
 
-const average = (...args) => args.reduce((x, y) => console.log(x+y/args.length)); 
+const average = (...args) => console.log(args.reduce((x,y) => x + y) / args.length);
 
-average(...rest);
+average(...grades);
